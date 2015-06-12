581efcb6ed68df10062ad20bba2032a4eb9fe232
riteshsangwan <sangwan.ritesh@yahoo.in>
2015-06-12 18:59:25 +0530

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,65 @@
+/**
+ * Angular application file.
+ * All the application bootstrapping, configuration, run goes here
+ *
+ * @author      ritesh
+ * @version     1.0
+ */
+(function() {
+  'use strict';
+  angular.module('billid', ['ionic', 'billid.controllers', 'billid.services', 'billidpoc'])
+  .constant('ENVIRONMENT', 'STAGING')
+  .constant('BASE_URL', '')
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: "templates/login.html",
+      controller: 'LoginController'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: "templates/login.html",
+      controller: 'RegisterController'
+    })
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppController'
+    })
+    .state('app.receipts', {
+      url: "/receipts",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/receipts.html",
+          controller: 'ReceiptsController'
+        }
+      }
+    })
+    .state('app.receipts.detail', {
+      url: "/:id",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/receiptDetail.html",
+          controller: 'ReceiptDetailController'
+        }
+      }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+  });
+}());
---
+++ www/js/services.js
@@ -0,0 +1,10 @@
+/**
+ * This file defines all the services for the application
+ *
+ * @author      ritesh
+ * @version     1.0
+ */
+(function() {
+  'use strict';
+  var services = angular.module('billid.services', []);
+}());
